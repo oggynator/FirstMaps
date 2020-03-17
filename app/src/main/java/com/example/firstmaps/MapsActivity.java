ebd8eef421fa97f78c8621c24c203b2fa2c9606b
oggynator <august.hejberg@gmail.com>
2020-03-17 13:45:36 +0100

    marker added

--- app/src/main/java/com/example/firstmaps/MapsActivity.java
+++ app/src/main/java/com/example/firstmaps/MapsActivity.java
@@ -2,8 +2,12 @@ package com.example.firstmaps;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,53 +18,58 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private Marker marker;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        builder = new AlertDialog.Builder(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(final LatLng latLng) {
+                // Alert dialog builder
+                final EditText editText = new EditText(MapsActivity.this);
+                editText.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setTitle("New marker");
+                builder.setMessage("What is on this location?");
+                builder.setView(editText)
+                        .setPositiveButton("Add marker", new DialogInterface.OnClickListener() {
+                            //onclick event to add the new marker
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                mMap.addMarker(new MarkerOptions().position(latLng).title(editText.getText().toString()));
+                                dialog.dismiss();
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .show();
+            }
+        });
+        addKEA();
+    }
 
 
+    //add kea to the Map
+    private void addKEA() {
         LatLng kea = new LatLng(55.703930, 12.537540);
         mMap.addMarker(new MarkerOptions().position(kea).title("Marker at KEA"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(kea));
-        mMap.setOnMapLongClickListener(this);
-
-    }
-
-
-    @Override
-    public void onMapLongClick(LatLng latLng) {
-
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        String markerPostition = String.valueOf(latLng);
-        Toast.makeText(this,
-                "Position of click " + markerPostition,
-                Toast.LENGTH_SHORT).show();
 
 
     }
+
+
 }
